66d562deaf2ca80eefb401165d70d5465f6c2091
Iryna I. Potiienko <iryna.potiienko@gmail.com>
2021-11-26 17:23:16 +0200

    Added gRPC Python client

---
+++ MyDBMS_gRPCclient/main.py
@@ -0,0 +1,38 @@
+import grpc
+
+# import the generated classes
+import tableProjection_pb2
+import tableProjection_pb2_grpc
+
+# open a gRPC channel
+channel = grpc.insecure_channel('localhost:5000')
+
+# create a stub (client)
+stub = tableProjection_pb2_grpc.TableProjectionStub(channel)
+
+# create a valid request message
+databaseName = input("Input database name: ")
+tableName = input("Input table name: ")
+columns = input("Input columns names: ")
+
+columnsNames = columns.split(", ")
+
+request = tableProjection_pb2.TableProjectionRequest(databaseName=databaseName, tableName=tableName,
+                                                     columnsNames=columnsNames)
+
+print("\n-----Table Projection----",
+      "\ndatabaseName: ", databaseName,
+      "\ntableName: ", tableName,
+      "\ncolumnsNames: ", columnsNames)
+
+# make the call
+response = stub.FindTableProjection(request)
+
+print("\n\nServerResponse:")
+if response.ErrorMessage != "":
+    print("Table doesn't exist in database!")
+else:
+    i = 0
+    for row in response.projectionRows:
+        print("Row", i, row.row)
+        i += 1
